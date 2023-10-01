481f83a2615276d984e97dbc89e6bfe16dd8ec70
Chad Rossouw <chadrossouw7247@gmail.com>
2023-09-30 22:17:16 -0400

    finished lab2

--- Driver.java
+++ Driver.java
@@ -1,16 +1,27 @@
+import java.util.Arrays;
+
 public class Driver {
     public static void main(String[] args) {
         Polynomial p = new Polynomial();
         System.out.println(p.evaluate(3));
-        double[] c1 = { 6, 0, 0, 5 };
-        Polynomial p1 = new Polynomial(c1);
-        double[] c2 = { 0, -2, 0, 0, -9 };
-        Polynomial p2 = new Polynomial(c2);
+        double[] c1 = { 6, 5 };
+        int[] e1 = {0, 3};
+        Polynomial p1 = new Polynomial(c1, e1);
+        double[] c2 = { -2, -9 };
+        int[] e2 = {1, 4};
+        Polynomial p2 = new Polynomial(c2, e2);
         Polynomial s = p1.add(p2);
         System.out.println("s(0.1) = " + s.evaluate(0.1));
+        System.out.println(Arrays.toString(s.coefficients));
+        System.out.println(Arrays.toString(s.exponents));
         if (s.hasRoot(1))
             System.out.println("1 is a root of s");
         else
             System.out.println("1 is not a root of s");
+
+        // Expected: -12x - 64x^4 - 45x^7
+        Polynomial t = p1.multiply(p2);
+        System.out.println(Arrays.toString(t.coefficients));
+        System.out.println(Arrays.toString(t.exponents));
     }
 }
--- Polynomial.java
+++ Polynomial.java
@@ -1,34 +1,115 @@
-import java.util.Arrays;
+import java.util.*;
 
 public class Polynomial {
     double[] coefficients;
+    int[] exponents;
 
     public Polynomial() {
         this.coefficients = new double[1];
         this.coefficients[0] = 0;
+        this.exponents = new int[1];
+        this.exponents[0] = 0;
     }
 
-    public Polynomial(double[] coeffs) {
-        this.coefficients = coeffs;
+    public Polynomial(double[] coefficients, int[] exponents) {
+        this.coefficients = coefficients;
+        this.exponents = exponents;
     }
 
     public Polynomial add(Polynomial poly) {
-        int coeffCount = Math.max(this.coefficients.length, poly.coefficients.length);
-        double[] newCoefficients = new double[coeffCount];
+        // Create tracking array of where exponents are needed. A needed exponent will have 1 at its index, otherwise 0.
+        int largestLen = Math.max(this.exponents[this.exponents.length - 1], poly.exponents[poly.exponents.length - 1]);
+        int[] newExponentTracker = new int[largestLen + 1];
+        Arrays.fill(newExponentTracker, 0);
+        int needed = 0;
+
+        // Populate tracker
+        for (int i : this.exponents) {
+            newExponentTracker[i] = 1;
+            needed++;
+        }
+        for (int i : poly.exponents) {
+            if (newExponentTracker[i] == 0) {
+                newExponentTracker[i] = 1;
+                needed++;
+            }
+        }
+
+        // Populate new exponents
+        int[] newExponents = new int[needed];
+        int j = 0;
+        for (int i = 0; i < newExponentTracker.length; i++) {
+            if (newExponentTracker[i] == 1) {
+                newExponents[j] = i;
+                j++;
+            }
+        }
+
+        // Populate new coefficients
+        double[] newCoefficients = new double[needed];
         Arrays.fill(newCoefficients, 0);
 
-        for (int i = 0; i < coeffCount; i++) {
-            if (i < this.coefficients.length) newCoefficients[i] += this.coefficients[i];
-            if (i < poly.coefficients.length) newCoefficients[i] += poly.coefficients[i];
-        } 
+        for (int i = 0; i < largestLen; i++) {
+            // Get index of exponent in newExponents
+            // Add coefficients to newCoefficients
+            if (i < this.coefficients.length) {
+                int idx = Arrays.binarySearch(newExponents, this.exponents[i]);
+                newCoefficients[idx] += this.coefficients[i];
+            }
+            if (i < poly.coefficients.length) {
+                int idx = Arrays.binarySearch(newExponents, poly.exponents[i]);
+                newCoefficients[idx] += poly.coefficients[i];
+            }
+        }
 
-        return new Polynomial(newCoefficients);
+        return new Polynomial(newCoefficients, newExponents);
+    }
+
+    public Polynomial multiply(Polynomial poly) {
+        int largestPower = this.exponents[this.exponents.length - 1] * poly.exponents[poly.exponents.length - 1];
+        int[] newExponentTracker = new int[largestPower + 1];
+        Arrays.fill(newExponentTracker, 0);
+        int needed = 0;
+
+        // Populate tracker
+        for (int i : this.exponents) {
+            for (int j : poly.exponents) {
+                int idx = i + j;
+                if (newExponentTracker[idx] == 0) {
+                    newExponentTracker[idx] = 1;
+                    needed++;
+                }
+            }
+        }
+
+        // Populate new exponents
+        int[] newExponents = new int[needed];
+        int idx = 0;
+        for (int i = 0; i < newExponentTracker.length; i++) {
+            if (newExponentTracker[i] == 1) {
+                newExponents[idx] = i;
+                idx++;
+            }
+        }
+
+        // Populate new coefficients
+        double[] newCoefficients = new double[needed];
+        Arrays.fill(newCoefficients, 0);
+
+        for (int i = 0; i < this.exponents.length; i++) {
+            for (int j = 0; j < poly.exponents.length; j++) {
+                int expIdx = Arrays.binarySearch(newExponents, this.exponents[i]+poly.exponents[j]);
+                newCoefficients[expIdx] += this.coefficients[i] * poly.coefficients[j];
+            }
+        }
+
+        return new Polynomial(newCoefficients, newExponents);
     }
 
     public double evaluate(double x) {
         double res = 0;
-        for (int i = 0; i < coefficients.length; i++) {
-            res += coefficients[i] * Math.pow(x, i);
+        for (int i = 0; i < this.coefficients.length; i++) {
+            res += this.coefficients[i] * Math.pow(x, this.exponents[i]);
         }
         return res;
     }
